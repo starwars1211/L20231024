639ec1f4d580f46e55e11048c10b456064a616a9
starwars1211 <js34313431@naver.com>
2023-10-24 16:44:53 +0900

    20231024 강의노트

--- main.cpp
+++ main.cpp
@@ -1,16 +1,53 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-	int A = 1
-	int B = 2
-	cout << "Him Him";
-	cout << "OOOOPs";
-	cout << "OOPS";
-	cout << "this is moment";
-	cout << "hellow world";
+	
+	
+	int PlayerX = 1;
+	int PlayerY = 1;
+	char PlayerShape = 'p';
+	bool bIsRunning = true;
+
+	while (bIsRunning)
+	{
+		int Key = _getch();
+
+		system("cls");
+
+		if (Key == 'a' || Key == 'A')
+		{
+			cout << "왼쪽" << endl;
+			PlayerX--;
+		}
+		else if (Key == 'd' || Key == 'D')
+		{
+			cout << "오른쪽" << endl;
+			PlayerX++;
+		}
+		else if (Key == 'w' || Key == 'W')
+		{
+			cout << "위" << endl;
+			PlayerY--;
+		}
+		else if (Key == 's' || Key == 'S')
+		{
+			cout << "아래" << endl;
+			PlayerY++;
+		}
+		else if (Key == 'q' || Key == 'Q')
+		{
+			cout << "종료" << endl;
+			bIsRunning = false;
+		}
+		cout << PlayerX << "," << PlayerY << endl;
+		
+		cout << PlayerShape;
+	}
 	
 	return 0;
 }
